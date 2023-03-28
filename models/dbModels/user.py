15312d4316b45cfeaeef74f7e46862353a0cd10f
AbhiBillion <51992409+AbhiBillion@users.noreply.github.com>
2023-03-28 22:59:04 +0530

    Add files via upload

--- models/dbModels/user.py
+++ models/dbModels/user.py
@@ -8,22 +8,22 @@ class OutModel(BaseModel):
     data: Any
 
 class address(BaseModel):
-    houseName: str
-    streetName: str
-    streetName2: str
-    city: str
-    district: str
-    state: str
-    zipcode: str
-    longitude: str
-    latitute: str
+    houseName: str = Field(example="Dream home")
+    streetName: str = Field(example="Street 32/B")
+    streetName2: str = Field(example="MG Road")
+    city: str = Field(example="Menaka")
+    district: str = Field(example="Ernakulam")
+    state: str = Field(example="Kerala")
+    zipcode: str = Field(example="62001")
+    longitude: str = Field(example="40.748817")
+    latitute: str = Field(example="-73.985428")
 
 class getAddressByDistance(BaseModel):
-    rangeKm : str
-    longitude: str
-    latitute: str
+    rangeKm : str = Field(example="10")
+    longitude: str = Field(example="40.748817")
+    latitute: str = Field(example="-73.985428")
 
 class updateAdr(BaseModel):
-    id : str
-    fieldName : str
-    value : str
+    id : str = Field(example="1")
+    fieldName : str = Field(example="houseName")
+    value : str = Field(example="Dream home")
