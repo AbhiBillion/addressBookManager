acf00c59978efb33f3e34a35dfbbe151ccb8f280
AbhiBillion <51992409+AbhiBillion@users.noreply.github.com>
2023-03-28 22:58:38 +0530

    Add files via upload

--- routes/dbRoutes/user.py
+++ routes/dbRoutes/user.py
@@ -1,10 +1,24 @@
 from fastapi import APIRouter
 from models.dbModels import user as u
 from helpers import dbTasks as db
-import geopy.distance
+from geopy.distance import distance
+import os
+from datetime import datetime
+
+if not os.path.exists('log'):
+    os.makedirs('log')
 
 routes = APIRouter()
 
+#to log error
+async def logError(text):
+    with open(f"errorLog/{str(datetime)}", "w") as f:
+        f.write(text)
+
+'''
+SAVE ADDRESS
+input parameter validaton with models
+'''
 @routes.post("/saveAddress", response_model=u.OutModel)
 async def saveAddress(saveAdr: u.address):
     try:
@@ -22,8 +36,15 @@ async def saveAddress(saveAdr: u.address):
                        status_code=400,
                        comment="Data insert failed",
                        data=e)
+        await logError(str(e))
         return out
 
+'''
+GET ADDRESS
+enter the preferred range in Km
+then give the source cordinates
+based on the source coordinates and rnge, it will show the matching records
+'''
 @routes.get("/getAddressInfo", response_model= u.OutModel)
 async def getAddress(getInfoAdr: u.getAddressByDistance):
     try:
@@ -33,13 +54,14 @@ async def getAddress(getInfoAdr: u.getAddressByDistance):
         addRangeList =[]
         for a in addressList:
             cord2 = (a['long'],a['lat'])
-            r = geopy.distance.geodesic(cord1,cord2).km
+            r = distance(cord1,cord2).km
             if r < range:
                 addRangeList(a)
         out = u.OutModel(status="success",
                         status_code=200,
                         comment="Data fetch successfully",
                         data=addRangeList)
+        await logError(str(e))
         return out
     except Exception as e:
         out = u.OutModel(status="failure",
@@ -48,6 +70,10 @@ async def getAddress(getInfoAdr: u.getAddressByDistance):
                         data='')
         return out
     
+'''
+UPDATE ADDRESS
+give the id, field name and the value
+'''
 @routes.put("/updateAddress", response_model= u.OutModel)
 async def recreate_stocklist(updateAdr : u.updateAdr):
     try:
@@ -62,8 +88,12 @@ async def recreate_stocklist(updateAdr : u.updateAdr):
                         status_code=400,
                         comment="Data update failed",
                         data='')
+        await logError(str(e))
         return out
 
+'''
+DELETE ADDRESS
+enter the id to delete the record from DB'''
 @routes.delete("/removeAddress", response_model= u.OutModel)
 async def deleteAddress(id : str):
     try:
@@ -78,6 +108,7 @@ async def deleteAddress(id : str):
                         status_code=400,
                         comment="Data delete failed",
                         data='')
+        await logError(str(e))
         return out
 
 
